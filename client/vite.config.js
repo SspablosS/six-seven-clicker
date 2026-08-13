2a153230b67aff8c5b888306cdf9034bd5823ef6
Pablo <pavelmeshkov2004@gmail.com>
2026-08-13 21:05:47 +0300

    refactoring

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,9 @@
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -22,4 +22,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
